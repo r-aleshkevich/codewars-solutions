dfd84e0425a162d38a10f0f50d039c811efd13b8
Роман Алешкевич <aleshkevich.roma2953@gmail.com>
2026-08-08 21:39:10 +0300

    Add solution - CodeHub

---
+++ 6-kyu/unique-in-order/unique-in-order.py
@@ -0,0 +1,9 @@
+def unique_in_order(sequence):
+    result = []
+    for item in sequence:
+        if result == [] or item != result[-1]:
+            result.append(item)
+            
+    return result
+        
+        
