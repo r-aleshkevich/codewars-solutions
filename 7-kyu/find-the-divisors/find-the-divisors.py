f27765fa8c6b50584a379b3fcaaa3a74fa6b8afa
Роман Алешкевич <aleshkevich.roma2953@gmail.com>
2026-09-01 17:55:30 +0300

    Add solution - CodeHub

---
+++ 7-kyu/find-the-divisors/find-the-divisors.py
@@ -0,0 +1,8 @@
+def divisors(integer):
+    res = []
+    for i in range(2, integer ):
+        if integer % i == 0 :
+            res.append(i)
+    if len(res) ==  0:
+        return f"{integer} is prime"
+    return res
